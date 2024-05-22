5a95b2408e65b31f46a4ee8de49f8f4434d5f2d7
christian.orr@diamond.ac.uk <christian.orr@diamond.ac.uk>
2024-05-22 08:44:36 +0000

    renaming things

--- client_code/questionnaire/__init__.py
+++ client_code/questionnaire/__init__.py
@@ -10,6 +10,33 @@ class questionnaire(questionnaireTemplate):
 
     # Any code you write here will run before the form opens.
 
-  def text_box_4_show(self, **event_args):
+  def cryoprotectant_show(self, **event_args):
     """This method is called when the TextBox is shown on the screen"""
+    pass
+
+  def cryoprotectantCheckBox_change(self, **event_args):
+    """This method is called when this checkbox is checked or unchecked"""
+    if self.cryoprotectantCheckBox.checked:
+      self.cryoprotectant.enabled = True
+    elif not self.cryoprotectantCheckBox.checked:
+      self.cryoprotectant.text = ""
+      self.cryoprotectant.enabled = False
+    pass
+
+  def localMountCheckBox_change(self, **event_args):
+    """This method is called when this checkbox is checked or unchecked"""
+    if self.localMountCheckBox.checked:
+      self.mountsSize.text = ""
+      self.mountsSize.enabled = False
+      self.handingToolsCheckBox.checked = False
+      self.handingToolsCheckBox.enabled = False
+      self.address.text = ""
+      self.address.enabled = False
+      self.EORI.text = ""
+      self.EORI.enabled = False
+    elif not self.localMountCheckBox.checked:
+      self.mountsSize.enabled = True
+      self.handingToolsCheckBox.enabled = True
+      self.address.enabled = True
+      self.EORI.enabled = True
     pass
